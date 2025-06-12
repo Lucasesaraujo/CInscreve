d7830ca62bde287670a45811e77d01984537c354
millena <millenaellensilva@gmail.com>
2025-06-12 20:18:03 -0300

    ⚡ feat: Criação da tipografia

---
+++ frontend/src/components/Tipografia.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Tipografia() {
+  return (
+    <div>Tipografia</div>
+  )
+}
+
+/*
+comandos importantes:
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+node server.js
+npx prisma studio
+*/
