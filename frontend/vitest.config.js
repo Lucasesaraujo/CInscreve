1b4d4028e9bd4056951122efe044c2682cebb62c
Vinicius Lidington <vlidington@gmail.com>
2025-08-27 16:05:17 -0300

    deploy

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -1,12 +1,17 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/services/setupTests.js'
-  }
+  base: "/cinscreve/", // Caminho base para o frontend
+  preview: {
+    port: 5002, // Porta do frontend no ambiente de preview
+    strictPort: true,
+  },
+  server: {
+    port: 5002, // Porta do servidor de desenvolvimento
+    strictPort: true,
+    host: true, // Permite acesso externo
+    origin: "http://0.0.0.0:5002", // Origem para o servidor
+  },
 });
