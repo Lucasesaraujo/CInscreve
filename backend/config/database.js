5be9c3b7e5a6f7a2da6f2e68103a493dcd3ceb87
Wesley Silveira <wslc@cin.ufpe.br>
2025-08-27 19:59:28 -0300

    tentativa infinita de database

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,14 +4,17 @@ const logger = require('./logger');
 require('dotenv').config();
 
 async function connectDB() {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        logger.info("Conexão realizada com sucesso!");    
-
-    } catch (error) {
-        logger.error('ERRO ao conectar!', error.message, error);
+    while (true) {
+        try {
+            await mongoose.connect(process.env.MONGO_URI);
+            logger.info("Conexão realizada com sucesso!");
+            break; // Sai do loop quando conectar
+        } catch (error) {
+            logger.error('ERRO ao conectar!', error.message, error);
+            logger.info('Tentando novamente em 5 segundos...');
+            await new Promise(resolve => setTimeout(resolve, 5000));
+        }
     }
-
 }
 
 module.exports = connectDB;
