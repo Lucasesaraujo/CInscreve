bbceb6f5e68314cad8455caeb65a4d596bce2b08
Vinicius Lidington <vlidington@gmail.com>
2025-08-27 16:58:14 -0300

    deploy

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,34 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-app.use(cors({
-  origin: true, // Permite qualquer origem
-  credentials: true
-}));
+const allowedOrigins = [
+    'http://localhost:5002', 
+    'http://172.18.0.3:5002', 
+    'http://172.18.0.2:5002', 
+    'http://vm-cinboraimpactar2.cin.ufpe.br', 
+    'http://vm-cinboraimpactar2.cin.ufpe.br/cinscreve/', 
+    'http://vm-cinboraimpactar2.cin.ufpe.br:5002', 
+    'https://cinboraimpactar.cin.ufpe.br/cinscreve/',
+    'https://cinboraimpactar.cin.ufpe.br',
+    'https://vm-cinboraimpactar2.cin.ufpe.br',
+    'https://vm-cinboraimpactar2.cin.ufpe.br/cinscreve/',
+    'http://backend_cinscreve:3002/',
+    'http://frontend_cinscreve:5002/'
+  ]
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("Blocked by CORS:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use('/assets', express.static('assets')); 
 
