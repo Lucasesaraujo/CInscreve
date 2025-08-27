3b8d27d58467b4abf78db8e8801f4fca7830385b
Vinicius Lidington <vlidington@gmail.com>
2025-08-27 16:44:23 -0300

    deploy

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,22 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5002', 'http://172.18.0.3:5002', 'http://172.18.0.2:5002'],
+  origin: [
+    'http://localhost:5002', 
+    'http://172.18.0.3:5002', 
+    'http://172.18.0.2:5002', 
+    'http://vm-cinboraimpactar2.cin.ufpe.br', 
+    'http://vm-cinboraimpactar2.cin.ufpe.br/cinscreve/', 
+    'http://vm-cinboraimpactar2.cin.ufpe.br:5002', 
+    'https://cinboraimpactar.cin.ufpe.br/cinscreve/',
+    'https://cinboraimpactar.cin.ufpe.br',
+    'https://vm-cinboraimpactar2.cin.ufpe.br',
+    'https://vm-cinboraimpactar2.cin.ufpe.br/cinscreve/'
+  ],
   credentials: true
 }));
 
-app.use('/assets', express.static('assets'));
+app.use('/assets', express.static('assets')); 
 
 app.use(express.json());
 app.use(cookieParser());
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,9 @@
 // index.js
-
 const app = require('./app');
 const connectDB = require('./config/database');
 const logger = require('./config/logger');
-const cors = require('cors');
 
 const PORT = process.env.PORT || 3002; 
-
-
-// Permitir qualquer origem (CORS aberto)
-app.use(cors());
 
 connectDB();
 
